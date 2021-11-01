09a674c454e2c0571aeaa1ae2c7c74b71122407d
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-01 13:17:49 -0700

    remove anonymizer

--- src/do_it_all.py
+++ src/do_it_all.py
@@ -3,15 +3,14 @@ from create_project_structure import create_project_structure
 from get_course_data import create_course_output
 from anonymizer import anonymizer
 from interface import confirm_strict, print_success, shut_down
+import settings
+from settings import COURSE_ID
 
 def do_it_all():
-    COURSE_ID, new_analytics_folder, gradebook_folder = create_course_output()
-    confirm_strict(f"Please add any New Analytics downloads to {new_analytics_folder}. Confirm when complete enter [Y] or exit [N].")
-    confirm_strict(f"Please add your Gradebook export to {new_analytics_folder}. Confirm when complete enter [Y] or exit [N].")
-
+    create_course_output()
+    confirm_strict(f"Please add any New Analytics downloads to {settings.NEWANALYTICS_FOLDER}. Confirm when complete enter [Y] or exit [N].")
+    confirm_strict(f"Please add your Gradebook export to {settings.GRADEBOOK_FOLDER}. Confirm when complete enter [Y] or exit [N].")
     create_project_structure(COURSE_ID)
-    anonymizer(COURSE_ID)
-
     print_success("Done!")
 
 if __name__ == "__main__":

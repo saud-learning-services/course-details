ae025bff60b095cd4173bd37695249251a5e8684
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-10 13:20:29 -0800

    change folder structures

--- src/settings.py
+++ src/settings.py
@@ -6,15 +6,17 @@ COURSE_ID = helpers.get_course_code()
 
 # project structure folders 
 DATA_FOLDER = f'data/{COURSE_ID}'
-RAW_FOLDER = f'{DATA_FOLDER}/raw'
-PROJECT_FOLDER = f'{DATA_FOLDER}/project_data'
 
+# Raw Data
+RAW_FOLDER = f'{DATA_FOLDER}/raw'
 APIOUTPUT_FOLDER = f'{RAW_FOLDER}/api_output' 
 NEWANALYTICS_FOLDER = f'{RAW_FOLDER}/new_analytics_input'
 GRADEBOOK_FOLDER = f'{RAW_FOLDER}/gradebook_input'
 
-COURSESTRUCTURE_FOLDER = f'{PROJECT_FOLDER}/course_structure'
-USERDATA_FOLDER = f'{PROJECT_FOLDER}/user_data'
+# Project Data
+PROJECT_FOLDER = f'{DATA_FOLDER}/project_data'
+COURSESTRUCTURE_FOLDER = f'{PROJECT_FOLDER}/step1'
+USERDATA_FOLDER = f'{PROJECT_FOLDER}/step1'
 CLEANEDDATA_FOLDER = f'{PROJECT_FOLDER}/cleaned_data'
 TRANSFORMEDDATA_FOLDER = f'{PROJECT_FOLDER}/transformed_data'
 

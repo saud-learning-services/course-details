e66a4380c25942bf88b8964e4f0b1f06c54ab787
alisonmyers <alison.myers@sauder.ubc.ca>
2021-11-24 12:03:53 -0800

    organize functions

--- src/transform_for_tableau.py
+++ src/transform_for_tableau.py
@@ -28,6 +28,7 @@ def combine_course_structure():
 def combine_enrollment_and_new_analytics():
 
     new_analytics =  pd.read_csv(f'{CLEANEDDATA_FOLDER}/new_analytics.csv')
+    new_analytics['user_id'] = new_analytics['global_user_id'].apply(lambda x: x-112240000000000000)
 
     enrollment = pd.read_csv(f'{CLEANEDDATA_FOLDER}/enrollments.csv')
 
@@ -51,6 +52,8 @@ def combine_enrollment_and_new_analytics():
     student_analytics = student_analytics.query(f'filetype=={keepfiles}')
     student_analytics.to_csv(f'{TABLEAU_FOLDER}/student_analytics_noimages.csv', index=False)
 
+    return(student_analytics)
+
 def course_assignments_and_dates(student_analytics):
     first_date = student_analytics['access_date'].min()
     last_date = student_analytics['access_date'].max()
@@ -62,26 +65,33 @@ def course_assignments_and_dates(student_analytics):
     assignments = pd.read_csv(f'{CLEANEDDATA_FOLDER}/assignments.csv')
     assignments = assignments[['assignment_id', 'assignment_due_at', 'assignment_title']]
 
-        
-        
+
 
     assignments['date'] = assignments['assignment_due_at'].apply(lambda x: _parse_date_time(x))
     dates_df = dates_df.merge(assignments, on="date", how="left")
     dates_df.to_csv(f"{TABLEAU_FOLDER}/course_dates.csv")
 
+
+def clean_submissions_data():
     gb_info = pd.read_csv('f{CLEANEDDATA_FOLDER}/assignments.csv')
     gb_info = gb_info.drop(['assignment_description', 'assignment_workflow_state', 'assignment_rubric',\
                             'assignment_is_quiz', 'assignment_is_published'], axis=1)
 
-def clean_submissions_data():
-    submissions_df = pd.read_csv('cleaned_data/assignment_submissions.csv').drop('course_id', axis=1)
+    submissions_df = pd.read_csv(f'{CLEANEDDATA_FOLDER}/assignment_submissions.csv').drop('course_id', axis=1)
     submissions_df = submissions_df.merge(gb_info)
 
     submissions_df['percent_score'] = submissions_df.apply(lambda x: x['assignment_score']/x['assignment_points_possible'] if x['assignment_points_possible'] > 0 else None, axis=1)
-    submissions_df.to_csv('transformed_data/student_assignment_details.csv')
+    submissions_df.to_csv(f'{TABLEAU_FOLDER}/student_assignment_details.csv')
 
 def clean_gradebook_data():
-    gb_data = pd.read_csv('project_data_anonymized/user_data/gradebook_user_data.csv')
+    gb_data = pd.read_csv(f'{CLEANEDDATA_FOLDER}/gradebook_user_data.csv')
     gb_data['user_id'] = gb_data['ID_anon'] 
     final_scores = gb_data[['user_id', 'Current Score', 'Final Score']]
-    final_scores.to_csv("transformed_data/user_final_score.csv", index=False)
+    final_scores.to_csv(f"{TABLEAU_FOLDER}/user_final_score.csv", index=False)
+
+def main():
+    combine_course_structure()
+    stud_analytics = combine_enrollment_and_new_analytics()
+    course_assignments_and_dates(stud_analytics)
+    clean_submissions_data()
+    clean_gradebook_data()
